00cc206a093e6184463916af59bd4d22bb603551
maodesigns <mao.devv@gmail.com>
2023-03-10 02:17:03 +0100

    Added fetchSingleProduct

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -44,12 +44,25 @@ export const ProductsProvider = ({ children }) => {
     }
   };
 
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
+
   useEffect(() => {
     fetchProducts(url);
   }, []);
 
   return (
-    <ProductsContext.Provider value={{ ...state, closeSidebar, openSidebar }}>
+    <ProductsContext.Provider
+      value={{ ...state, closeSidebar, openSidebar, fetchSingleProduct }}
+    >
       {children}
     </ProductsContext.Provider>
   );
