b13afba5cbfd6dc431a29a92066c39c5c387de5d
maodesigns <mao.devv@gmail.com>
2023-03-15 11:26:02 +0100

    updated

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -14,8 +14,47 @@ const filter_reducer = (state, action) => {
     case LOAD_PRODUCTS:
       return {
         ...state,
-        all_products: { ...action.payload },
-        filtered_products: { ...action.payload },
+        all_products: [...action.payload],
+        filtered_products: [...action.payload],
+      };
+    case SET_GRIDVIEW:
+      return {
+        ...state,
+        grid_view: true,
+      };
+    case SET_LISTVIEW:
+      return {
+        ...state,
+        grid_view: false,
+      };
+    case UPDATE_SORT:
+      return {
+        ...state,
+        sort: action.payload,
+      };
+    case SORT_PRODUCTS:
+      const { sort, filtered_products } = state;
+      let tempProducts = [...filtered_products];
+
+      if (sort === "price-lowest")
+        tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+
+      if (sort === "price-highest")
+        tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+
+      if (sort === "name-a")
+        tempProducts = filtered_products.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+
+      if (sort === "name-z")
+        tempProducts = filtered_products.sort((a, b) =>
+          b.name.localeCompare(a.name)
+        );
+
+      return {
+        ...state,
+        filtered_products: tempProducts,
       };
 
     default:
