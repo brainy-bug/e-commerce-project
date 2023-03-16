33d8e1058b11f72598a55b3bacaaf09d7c2a88de
maodesigns <mao.devv@gmail.com>
2023-03-16 09:50:20 +0100

    Added Some Functionalities

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,8 +6,29 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+const increase = () => {
+  setAmount((oldAmount) => {
+    let tempAmount = oldAmount + 1;
+    if (tempAmount > stock) {
+      tempAmount = stock;
+    }
+    return tempAmount;
+  });
+};
+const decrease = () => {
+  setAmount((oldAmount) => {
+    let tempAmount = oldAmount - 1;
+    if (tempAmount < 1) {
+      tempAmount = 1;
+    }
+    return tempAmount;
+  });
+};
 
   return (
     <Wrapper>
@@ -30,7 +51,21 @@ const AddToCart = ({ product }) => {
           })}
         </div>
       </div>
-      <div className='btn-container'> </div>
+      <div className='btn-container'>
+        <AmountButtons
+          increase={increase}
+          decrease={decrease}
+          amount={amount}
+        />
+
+        <Link
+          to='/cart'
+          className='btn'
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
+          add to cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };
